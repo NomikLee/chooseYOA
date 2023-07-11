085484c1b8f17cc100143c652fbd5ef10c5b362f
Nomik Lee <s79318@icloud.com>
2023-07-11 19:15:24 +0800

    Initial Commit

---
+++ chooseYOA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chooseYOA
+//
+//  Created by Pinocchio on 2023/7/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
